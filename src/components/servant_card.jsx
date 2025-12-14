390702b4cd43726efdca914b88e52d7507b19a7c
SeanSM-NAB <SeanSMaseola@gmail.com>
2025-12-14 22:13:16 +0200

    Added images and displayed all data dynamically.

--- src/components/servant_card.jsx
+++ src/components/servant_card.jsx
@@ -4,17 +4,17 @@ export default function servant_card({ servant }) {
   return (
     <div class="servant-card">
       <div class="servant-image">
-        <img src="" />
+        <img src={servant.image} />
       </div>
       <div class="servant-data">
         <div>{servant.name}</div>
-        <div>rarity</div>
-        <div>class</div>
-        <div>role</div>
-        <div>cardType</div>
-        <div>skills</div>
-        <div>np</div>
-        <div>passives</div>
+        <div>{servant.rarity}</div>
+        <div>{servant.class}</div>
+        <div>{servant.role}</div>
+        <div>{servant.cardType}</div>
+        <div>{servant.skills[0]}</div>
+        <div>{servant.np}</div>
+        <div>{servant.passives[0]}</div>
       </div>
     </div>
   );
