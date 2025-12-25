70e7f54e73a652ebe90461ed6f6ebdc16caf4b05
SeanSM-NAB <SeanSMaseola@gmail.com>
2025-12-25 06:57:59 +0200

    Added the table element for the saved team classes

--- src/components/teams/TeamTable.jsx
+++ src/components/teams/TeamTable.jsx
@@ -3,7 +3,44 @@ import React from "react";
 export default function TeamTable() {
   return (
     <div>
-      <table></table>
+      <table>
+        <tr>
+          <th>Saber</th>
+          <th>Lancer</th>
+          <th>Archer</th>
+          <th>Caster</th>
+          <th>Assassin</th>
+          <th>Rider</th>
+          <th>Beserker</th>
+        </tr>
+        <tr>
+          <td>Salter</td>
+          <td>Ereshkigal</td>
+          <td>Emiya</td>
+          <td>Acse</td>
+          <td>Izo</td>
+          <td>Ushi</td>
+          <td>Hercules</td>
+        </tr>
+        <tr>
+          <td>Sigurd</td>
+          <td>Cu</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+        </tr>
+        <tr>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+        </tr>
+      </table>
     </div>
   );
 }
--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ServantTeamCard from "../components/teams/servantTeamCard";
+import TeamTable from "../components/teams/TeamTable";
 import "../App.css";
 
 export default function Teams() {
+  //Trying to create a function that will return an array of servants that match
+  //the role of the clicked slot
+  const handleGetServant = async () => {
+    try {
+      const data = await window.electronAPI.readItems();
+    } catch (error) {
+      console.log("Failed to load servants", error);
+    }
+  };
   return (
     <div>
       <div className="team-wrapper">
@@ -17,6 +27,9 @@ export default function Teams() {
           <ServantTeamCard />
           <ServantTeamCard />
           <ServantTeamCard />
+        </div>
+        <div className="team-table">
+          <TeamTable />
         </div>
       </div>
     </div>
