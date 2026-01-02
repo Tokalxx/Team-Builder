35735e169e01169acf9255cbf97c35db3fc415e8
SeanSM-NAB <SeanSMaseola@gmail.com>
2026-01-02 08:20:01 +0200

    Add in some styles on Team Page

--- src/components/teams/AddServant.jsx
+++ src/components/teams/AddServant.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import "./teams-style.css";
 
 export default function AddServant({ servant }) {
   return (
     <div>
       {}
-      <div className="">
-        <img />
-        <h3>{servant.name}</h3>
-        <label>Role</label>
-        <label>Card Type</label>
+      <div className="servant-card_team">
+        <img src={servant.image} />
+        <div className="servant-card_team--details">
+          <h3>{servant.name}</h3>
+          <br />
+          <label>{servant.role.join(", ")}</label>
+          <br />
+          <label>Card Type</label>
+        </div>
       </div>
     </div>
   );
