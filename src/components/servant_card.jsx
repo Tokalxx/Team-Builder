36a6ae292fbb661614a34f9b0866eddff4ec16e3
SeanSM-NAB <SeanSMaseola@gmail.com>
2026-01-12 13:17:56 +0200

    Enhanced the card design on the Servant Page

--- src/components/servant_card.jsx
+++ src/components/servant_card.jsx
@@ -5,17 +5,24 @@ export default function servant_card({ servant, onDelete, onEdit }) {
   return (
     <div className="servant-card">
       <div className="servant-image">
-        <img src={servant.image} />
+        <img src={servant.image} alt={servant.name} />
       </div>
+
       <div className="servant-data">
-        <div>{servant.name}</div>
-        <div>{servant.class}</div>
-        <button className="delete_servant" onClick={() => onDelete(servant.id)}>
-          X
-        </button>
-        <button className="update_servant" onClick={onEdit}>
-          UP
-        </button>
+        <div className="servant-name">{servant.name}</div>
+        <div className="servant-class">{servant.class}</div>
+
+        <div className="servant-actions">
+          <button className="update_servant" onClick={onEdit}>
+            Update
+          </button>
+          <button
+            className="delete_servant"
+            onClick={() => onDelete(servant.id)}
+          >
+            ✕
+          </button>
+        </div>
       </div>
     </div>
   );
